81132472f1d60b23c657d0dcc53938360df21ae0
kashyapjhaveri <33370217+kashyapjhaveri@users.noreply.github.com>
2020-01-29 19:43:20 -0500

    [KashyapJhaveri] Maintained null pointer exception in every method

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -6,6 +6,8 @@
 package com.lambton;
 
 import javax.print.DocFlavor;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 
 /**
@@ -54,37 +56,70 @@ public class LambtonStringTools
     //2 - FORMAT INITIALS OF STRING
     public static String initials(String s) 
     {
-        return null;
+        String splittedArrayString[] = splitTheStringIntoArray(s);
+        /*String ini="";
+        for (String word:splittedArray)
+        {
+            char temp[]=word.toCharArray();
+            ini=ini.concat(temp[0]);
+        }*/
+
+        if (s==null)
+        {
+            return null;
+        }
+        else {
+            char splittedArray[] = s.toCharArray();
+            String temp = "";
+            ArrayList<Character> ini = new ArrayList<Character>();
+
+            ini.add(splittedArray[0]);
+            ini.add('.');
+
+            for (int i = 0; i < splittedArray.length; i++) {
+                if (splittedArray[i] == ' ') {
+                    ini.add(splittedArray[i + 1]);
+                    ini.add('.');
+                }
+            }
+
+            for (int i = 0; i < ini.size() - 2; i++) {
+                temp = temp + ini.get(i);
+            }
+            temp = temp + splittedArrayString[splittedArrayString.length - 1];
+            return temp;
+        }
     }
         
     //3 - FIND MOST FREQUENT CHARACTER FROM STRING
     public static String mostFrequent(String s) 
     {
-        char temp[]=s.toCharArray();
-
-        HashMap<Character,Integer> frequentLetters = new HashMap<Character, Integer>();
-
-        for (int i=0;i<s.length();i++)
+        if (s==null)
         {
-            if (frequentLetters.containsKey(temp[i]))
-            {
-                int tempValue= frequentLetters.get(temp[i]);
-                tempValue++;
-                frequentLetters.put(temp[i],tempValue);
-            }
-            else
-            {
-                frequentLetters.put(temp[i],1);
-            }
+            return null;
         }
+        else {
+            char temp[] = s.toCharArray();
 
+            HashMap<Character, Integer> frequentLetters = new HashMap<Character, Integer>();
+
+            for (int i = 0; i < s.length(); i++) {
+                if (frequentLetters.containsKey(temp[i])) {
+                    int tempValue = frequentLetters.get(temp[i]);
+                    tempValue++;
+                    frequentLetters.put(temp[i], tempValue);
+                } else {
+                    frequentLetters.put(temp[i], 1);
+                }
+            }
+            return null;
+        }
 
 
         /*for (int a:frequentLetters.values())
         {
             System.out.println(a);
         }*/
-        return  null;
     }
     
     //4 - CONVERT BINARY NUMBER TO DECIMALS
@@ -100,23 +135,26 @@ public class LambtonStringTools
     //5 - REPLACING SUBSTRING WITH NEW STRING IN EXISTING STRING
     public static String replaceSubString(String originalString, String findString, String newString) 
     {
-
-        String stringInWords[]=splitTheStringIntoArray(originalString);
-        String temp=" ";
-        for (int i=0;i<stringInWords.length;i++)
+        if (originalString==null || findString==null || newString==null)
         {
+            return null;
+        }
+        else {
+            String stringInWords[] = splitTheStringIntoArray(originalString);
+            String temp = " ";
+            for (int i = 0; i < stringInWords.length; i++) {
 
-            if (stringInWords[i].compareToIgnoreCase(findString)==0)
-            {
-                stringInWords[i]=newString;
+                if (stringInWords[i].compareToIgnoreCase(findString) == 0) {
+                    stringInWords[i] = newString;
+                }
             }
+
+            for (String t : stringInWords) {
+
+                temp = temp + t + " ";
+            }
+
+            return temp;
         }
-
-        for (String t:stringInWords) {
-
-            temp=temp+t+" ";
-        }
-
-        return temp;
     }
 }
